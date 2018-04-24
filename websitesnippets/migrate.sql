bb8ca581f7555bfeee92c4bfea35970240cd0925
mikesoloviev <38215251+mikesoloviev@users.noreply.github.com>
2018-04-24 14:07:57 -0400

    Delete migrate.sql

--- websitesnippets/migrate.sql
+++
@@ -1,7 +0,0 @@
-CREATE SCHEMA `mywebapp`;
-USE `mywebapp`;
-CREATE TABLE `account` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `Username` VARCHAR(100) NOT NULL DEFAULT '',
-  `Password` VARCHAR(100) NOT NULL DEFAULT '',
-  PRIMARY KEY (`Id`));
